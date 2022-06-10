fd2ea4d850fca25693cdd7434f08d2e60ae9d372
unknown <cristian.ionel1@isa.utm.md>
2022-06-10 03:01:59 +0300

    redone cnf script, solved bugs, reformat output

--- lab4/cnf.py
+++ lab4/cnf.py
@@ -1,157 +1,190 @@
-import copy
+from itertools import product
+import pprint
 
-gramar = {
+grammar_dict = {
     'S': ['AC', 'bA', 'B', 'aA'],
-    'A': ['X', 'aS', 'ABAb'],
+    'A': ['$', 'aS', 'ABAb'],
     'B': ['a', 'AbSA'],
     'C': ['abC'],
-    'D': ['AB']
+    'D': ['AB'],
 }
+
+print('')
+print("Initial grammar : ")
+print("^"*70)
+pprint.pprint(grammar_dict, sort_dicts=False)
+
 term_syms = ['a', 'b']
 unterm_syms = ['S', 'A', 'B', 'C', 'D']
-empty_sym = 'X'
+empty_sym = '$'
 start_sym = 'S'
 
-pool = {
-    'ind': 0,
-    'pool': 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-}
+null_syms = set()
+flag = False
 
-def gen_unterm(pool, used_unterm_syms):
-    pool_array = pool['pool']
-    cur_ind = pool['ind']
-    cur_unterm_syms = pool_array[cur_ind]
-    while(cur_unterm_syms in used_unterm_syms) and cur_ind < 26:
-        cur_ind += 1
-        cur_unterm_syms = pool_array[cur_ind]
-    pool['ind'] = cur_ind
-    if cur_ind >= 26:
-        return None
-    else:
-        return cur_unterm_syms
+while not flag:
+    flag = True
+    for k, v in grammar_dict.items():
+        if k in null_syms:
+            continue
+        for string in v:
+            if string == empty_sym or all([nonterminal in null_syms for nonterminal in string]):
+                null_syms.add(k)
+                flag = False
 
-convert_term_syms_dict = {}
-for k,p in gramar.items():
-    for p_ind in range(0, len(p)):
-        cur_p = p[p_ind]
-        for ind in range(0, len(cur_p)):
-            if cur_p[ind] in term_syms:
-                if cur_p[ind] in convert_term_syms_dict:
-                    p[p_ind] = cur_p[0:ind] + convert_term_syms_dict[cur_p[ind]] + cur_p[(ind+1):]
-                    cur_p = p[p_ind]
-                else:
-                    convert_term_syms_dict[cur_p[ind]] = gen_unterm(pool, unterm_syms)
-                    p[p_ind] = cur_p[0:ind] + convert_term_syms_dict[cur_p[ind]] + cur_p[(ind+1):]
-                    unterm_syms.append(p[p_ind][ind])
-                    cur_p = p[p_ind]
+print('')
+print("Nullable Symbols : ")
+print("^"*70)
+pprint.pprint(null_syms, sort_dicts=False)
 
-for p,k in convert_term_syms_dict.items():
-    gramar[k] = [p]
-print("TERM Result: "+str(gramar))
+def filter(word, to_replace, replacement):
+    options = [(c,) if c != to_replace else (to_replace, replacement) for c in word]
+    return ("".join(o) for o in product(*options))
 
-def shink_grammar(gramar, unterm_syms, pool):
-    new_p_dict = {}
-    shink_count = 0
-    for k,p in gramar.items():
-        for ind in range(0, len(p)):
-            if len(p[ind]) > 2:
-                new_unterm = gen_unterm(pool, unterm_syms)
-                unterm_syms.append(new_unterm)
-                new_p_dict[new_unterm] = p[ind][1:]
-                p[ind] = p[ind][0]+new_unterm
-    for k,p in new_p_dict.items():
-        gramar[k] = [p]
-        shink_count += 1
-    return shink_count
-
-while(shink_grammar(gramar, unterm_syms, pool) > 0):
-    pass
-
-print("Shrink out: "+str(gramar))
-
-null_syms = [empty_sym]
-null_sym_count = 0xff
-while(null_sym_count > 0):
-    null_sym_count = 0
-    for k,p in gramar.items():
-        for cur_p in p:
-            is_null_p = True
-            for sym in cur_p:
-                is_null_p = is_null_p and (sym in null_syms)
-            if is_null_p and (k not in null_syms):
-                null_syms.append(k)
-                null_sym_count += 1
-print("Nullable symbols: "+str(null_syms))
-
-def gen_mask_set(null_syms, p, empty_sym):
-    empty = [p]
-    item_count = 0xff
-    while(item_count > 0):
-        item_count = 0
-        for ind in range(0, len(empty)):
-            cur_item = empty[ind]
-            for cur_ind in range(0, len(cur_item)):
-                if cur_item[cur_ind] in null_syms:
-                    new_p = cur_item[0:cur_ind]+cur_item[(cur_ind+1):]
-                    if len(new_p)==0:
-                        new_p = empty_sym
-                    if new_p not in empty:
-                        empty.append(new_p)
-                        item_count += 1
-    return empty
-
-new_grammar_dict = {}
-for k,p in gramar.items():
-    new_grammar_dict[k] = copy.copy(p)
+for k,p in grammar_dict.items():
     for ind in range(0, len(p)):
         cur_p = p[ind]
-        empty_p = gen_mask_set(null_syms, cur_p, empty_sym)
-        new_grammar_dict[k] += empty_p
-    new_grammar_dict[k] = list(set(new_grammar_dict[k]))
-    if empty_sym in new_grammar_dict[k]:
-        new_grammar_dict[k].remove(empty_sym)
+        for null_var in null_syms:
+            empty_p = filter(cur_p, null_var, '')
+        grammar_dict[k] += empty_p
+    grammar_dict[k] = list(set(grammar_dict[k]))
+    if empty_sym in grammar_dict[k]:
+        grammar_dict[k].remove(empty_sym)
 
-print("No nullable: "+str(new_grammar_dict))
+print('')
+print("Remove epsilon productions : ")
+print("^"*70)
+pprint.pprint(grammar_dict, sort_dicts=False)
 
 unit_hash = {}
-unit_revert_hash = {}
-for k,p in new_grammar_dict.items():
+for k,p in grammar_dict.items():
     unit_hash[k] = []
     for cur_p in p:
         if cur_p in unterm_syms:
             unit_hash[k].append(cur_p)
-            if cur_p in unit_revert_hash:
-                unit_revert_hash[cur_p].append(k)
-            else:
-                unit_revert_hash[cur_p] = [k]
 
-not_run_items = []
-del_items = []
-replace_hash = {}
-for k,same_list in unit_hash.items():
-    if k in not_run_items:
-        continue
-    for same in same_list:
-        if k in unit_revert_hash[same]: # avoid A->B B->A case
-            not_run_items.append(same)
-        new_grammar_dict[k] += new_grammar_dict[same]
-        if same != start_sym:
-            del_items.append(same)
-        replace_hash[same] = k
-    new_grammar_dict[k] = list(set(new_grammar_dict[k]))
+unit_hash = dict( [(k,v) for k,v in unit_hash.items() if len(v)>0]) # remove empty keys
 
-unit_grammar_dict = {}
-for k,p in new_grammar_dict.items():
-    if k not in del_items:
-        unit_grammar_dict[k] = []
-        for ind in range(0, len(p)):
-            cur_p = p[ind]
-            for cur_ind in range(0, len(p[ind])):
-                if cur_p[cur_ind] in replace_hash:
-                    p[ind] = cur_p[0:cur_ind]+replace_hash[cur_p[cur_ind]]+cur_p[(cur_ind+1):]
-                    cur_p = p[ind]
-        for item in p:
-            if item not in unterm_syms:
-                unit_grammar_dict[k].append(item)
+for k, v in grammar_dict.items():
+    if k in unit_hash.keys():
+        for renaming in unit_hash[k]:
+            grammar_dict[k].remove(renaming)
+            if renaming in grammar_dict:
+                grammar_dict[k].extend(grammar_dict[renaming])
 
-print("CNF: "+str(unit_grammar_dict))
+for k, v in grammar_dict.items():
+    grammar_dict[k] = list(set(grammar_dict[k]))
+
+print("")
+print("Remove unit productions : ")
+print("^"*70)
+pprint.pprint(grammar_dict, sort_dicts=False)
+
+non_generating = []
+for key, value in grammar_dict.items():
+    generating = False
+    for rule in value:
+        if (all(r not in non_generating for r in rule)):
+            generating = True
+
+    if generating is False:
+        non_generating.append(key)
+
+for a in non_generating:
+    if a in grammar_dict.keys():
+        grammar_dict.pop(a, None)
+    for k, v in grammar_dict:
+        for rule in v:
+            if a in rule:
+                grammar_dict[k].remove(rule)
+
+print('')
+print("Remove nonproductive symbols : ")
+print("^"*70)
+pprint.pprint(grammar_dict, sort_dicts=False)
+
+reachables = [start_sym]
+for r in reachables:
+    rules = []
+    if r in unterm_syms:
+        rules = grammar_dict[r]
+    for rule in rules:
+        for symbol in rule:
+            if symbol not in reachables: 
+                reachables.append(symbol)
+
+all_symbols = set().union(unterm_syms, term_syms)
+
+unreachable = all_symbols - set(reachables)
+
+for a in unreachable:
+    if a in grammar_dict.keys():
+        grammar_dict.pop(a, None)
+
+print('')
+print("Remove nonreachable symbols : ")
+print("^"*70)
+pprint.pprint(grammar_dict, sort_dicts=False)
+
+split_dict = {}
+
+for k, v in grammar_dict.items():
+    if k not in split_dict.keys():
+        split_dict[k] = []
+    for rule in v:
+        split_dict[k].append(list(rule))
+
+counter = 1
+new_rules = {}
+for t in term_syms:
+    new_unterm = 'X' + str(counter)
+    # split_dict[new_unterm] = list([t])
+    new_rules[new_unterm] = list([t])
+    counter += 1
+
+for k, v in split_dict.items():
+    for rule in v:
+        if len(rule) > 1:
+            for i, char in enumerate(rule):
+                if char.islower():
+                    myKey = [key for key, value in new_rules.items() if value == [char]]
+                    rule[i] = myKey.pop()
+
+counter = 1
+
+while True:
+    flag = True
+    for key, value in split_dict.items():
+        for a in value:
+            if len(a) > 2:
+                new_rule = [a[0], a[1]]
+                if new_rule not in new_rules.values():
+                    flag = False
+                    new_rules['Y' + str(counter)] = new_rule
+                    counter += 1
+
+    for k, v in split_dict.items():
+        for i, a in enumerate(split_dict[k]):
+            # print(split_dict[k][i])
+            if len(a) > 2:
+                toFind = [split_dict[k][i][0], split_dict[k][i][1]]
+                for k_, v_ in new_rules.items():
+                    if toFind == v_:
+                        split_dict[k][i] = split_dict[k][i][1:]
+                        split_dict[k][i][0] = k_
+                    
+    if flag: break
+
+for k, v in new_rules.items():
+    new_rules[k] = [''.join(new_rules[k])]
+
+for k, v in split_dict.items():
+    for i, a in enumerate(split_dict[k]):
+        split_dict[k][i] = ''.join(split_dict[k][i])
+
+split_dict.update(new_rules)
+
+print('')
+print("CNF : ")
+print("^"*70)
+pprint.pprint(split_dict, sort_dicts=False)
